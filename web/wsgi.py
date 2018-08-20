6d476d6809b19adf0a52d2e738ea7315282ed255
Stu Ryan <mikeryan56@gmail.com>
2018-08-20 10:58:22 -0400

    updates to support Python3.6-alpine for container build.

--- web/wsgi.py
+++ web/wsgi.py
@@ -2,4 +2,4 @@ from app import app
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=8080, debug=True) # This is the default port for Flask-Appbuilder
-    app.run(host='0.0.0.0', port=5000, debug=True) # Changing to port 5000 as it is the default for Flask
+    app.run(host='0.0.0.0', port=5000, debug=False) # Changing to port 5000 as it is the default for Flask
